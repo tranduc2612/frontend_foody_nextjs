a51f4fec2695e105eaccc2f1068696304fe09b3f
teddy.nguyen <teddy.nguyen@techvify.com.vn>
2024-12-13 15:52:06 +0700

    infinity scroll

--- app/_api/recipes/service.ts
+++ app/_api/recipes/service.ts
@@ -1,8 +1,8 @@
 import client from "@/app/_axios"
 import { Pagination, ResponseApi, ResponseError } from "@/app/_types/response"
 
-export const fetchListRecipes = async (input: GetRecipesPayload): Promise<ResponseApi<Pagination<Recipes>>> => {
-    const recipes = client<ResponseApi<Pagination<Recipes>>>(`/recipes/list?pageIndex=${Number(input.pageIndex)}&pageCount=${Number(input.pageCount)}`, {
+export const fetchListRecipes = async (input: GetRecipesPayload): Promise<ResponseApi<Pagination<Recipes[]>>> => {
+    const recipes = client<ResponseApi<Pagination<Recipes[]>>>(`/recipes/list?pageIndex=${Number(input.pageIndex)}&pageCount=${Number(input.pageCount)}`, {
         method: 'GET'
     })
     return recipes
