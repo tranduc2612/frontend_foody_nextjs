57e5f827e575edc61bcf474129d6030ed855a2b2
mint_duc <mintduc2612@gmail.com>
2025-01-04 23:11:25 +0700

    update cd

--- app/(main)/profile/[id]/page.tsx
+++ app/(main)/profile/[id]/page.tsx
@@ -1,14 +1,26 @@
-'use client'
-import { useEffect } from "react";
+// app/(main)/profile/[id]/page.tsx
 
-export default function User() {
-    useEffect(()=>{
-        console.log('hhh');
-    },[])
-    return (
-      <div className="">
-          User
-      </div>
-    );
-  }
-  
+// Định nghĩa IProps để tương thích với kiểu mà Next.js mong đợi
+interface IProps {
+  params: {
+    id: string;
+  };
+}
+
+// Hàm generateStaticParams để xác định các tham số động (id)
+export async function generateStaticParams() {
+  return [
+    { id: '1' },
+    { id: '2' },
+    { id: '3' },  // Thêm các id khác mà bạn muốn tạo trang tĩnh cho chúng
+  ];
+}
+
+export default function User({ params }: IProps) {
+  const { id } = params;
+  return (
+    <div>
+      User ID: {id}
+    </div>
+  );
+}
